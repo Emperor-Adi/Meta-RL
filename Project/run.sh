568d29d59c0e2ebbdc26e1963c5fbe860422e2e5
TheMonocledHamsterOnAdiPC <adithyah07@gmail.com>
2022-01-04 01:12:49 +0530

    hotfix

--- Project/run.sh
+++ Project/run.sh
@@ -5,7 +5,7 @@ environments=( "CartPole-v0" "CartPole-v1" "Acrobot-v1" "LunarLander-v2" )
 declare -A train_args
 declare -A test_args
 
-train_args=( ["CartPole-v0"]=200 ["Acrobot-v1"]=-150 ["CartPole-v1"]=450 ["LunarLander-v2"]=200 )
+train_args=( ["CartPole-v0"]=195 ["Acrobot-v1"]=-150 ["CartPole-v1"]=450 ["LunarLander-v2"]=200 )
 
 test_args=( ["CartPole-v0"]="200.0CartPole-v0.h5" ["CartPole-v1"]="500.0CartPole-v1.h5" ["Acrobot-v1"]="acrobot_actor-87.h5" ["LunarLander-v2"]="81.17627923613364lunar_actor.h5" )
 
